8056d32cc5d27955f774aca7932dad2dd283d4c5
bobilan <bobilan@gmail.com>
2023-10-26 13:24:19 +0200

    add basic tests for models

--- apps/user/tests/ut/test_models.py
+++ apps/user/tests/ut/test_models.py
@@ -49,3 +49,37 @@ class TestInterestModel(TestCase):
         self.assertEqual(str(interest), "photography")
 
 
+class TestCustomUserModel(TestCase):
+    def test_create_custom_user(self):
+        user = CustomUser.objects.create(email="test@example.com", password="pass")
+        self.assertEqual(user.email, "test@example.com")
+        self.assertTrue(user.check_password("pass"))
+
+
+class TestUserPhotoModel(TestCase):
+    def test_user_photo_creation(self):
+        user = CustomUser.objects.create(email="user@example.com", password="pass")
+        photo = UserPhoto.objects.create(user_id=user, photos="path/to/photo.jpg")
+        self.assertEqual(photo.user_id, user)
+        self.assertEqual(photo.photos, "path/to/photo.jpg")
+
+
+class TestGradeModel(TestCase):
+    def test_create_grade(self):
+        user1 = CustomUser.objects.create(email="user1@example.com", password="password")
+        user2 = CustomUser.objects.create(email="user2@example.com", password="password")
+        grade = Grade.objects.create(grade=5)
+        grade.user_id_given.add(user1)
+        grade.user_id_received.add(user2)
+        self.assertEqual(grade.grade, 5)
+        self.assertIn(user1, grade.user_id_given.all())
+        self.assertIn(user2, grade.user_id_received.all())
+
+
+class TestChildModel(TestCase):
+    def test_create_child(self):
+        user = CustomUser.objects.create(email="parent@example.com", password="pass")
+        child = Child.objects.create(parent_id=user, birthdate="2022-01-01", bio="Child bio")
+        self.assertEqual(child.parent_id, user)
+        self.assertEqual(child.birthdate, "2022-01-01")
+        self.assertEqual(child.bio, "Child bio")
