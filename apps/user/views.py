02a00ff7411e4b7c4b27bd7e2fed99e54c0f246e
bobilan <bobilan@gmail.com>
2023-11-07 14:29:34 +0100

    add user_own_page view, html. connect front to user data from db

--- apps/user/views.py
+++ apps/user/views.py
@@ -12,12 +12,12 @@ def login_view(request):
     error_message = None
     form = CustomAuthenticationForm()
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CustomAuthenticationForm(data=request.POST)
 
         if form.is_valid():
-            email = form.cleaned_data.get('email')
-            password = form.cleaned_data.get('password')
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
 
             user: Optional[CustomUser] = authenticate(
                 request,
@@ -27,11 +27,11 @@ def login_view(request):
 
             if user is not None:
                 login(request, user)
-                return redirect('logged_view')
+                return redirect("logged_view")
             else:
-                error_message = 'Incorrect email or password. Please try again.'
+                error_message = "Incorrect email or password. Please try again."
 
-    context = {'form': form, 'error_message': error_message}
+    context = {"form": form, "error_message": error_message}
 
     return render(request, "user/login.html", context)
 
@@ -44,5 +44,22 @@ def logged_view(request):
 @login_required
 def user_logout(request):
     logout(request)
-    return redirect('home_page')
+    return redirect("home_page")
 
+
+@login_required
+def user_profile_own(request):
+    user = request.user
+    context = {
+        "email": user.email,
+        "first_name": user.first_name,
+        "last_name": user.last_name,
+        "gender": user.gender,
+        "verified_status": user.verified_status,
+        "bio": user.bio,
+        "location": user.location,
+        "birthdate": user.birthdate,
+        "marital_status": user.marital_status,
+        "avatar": user.avatar.url if user.avatar else None,
+    }
+    return render(request, "user/user_page_own.html", context)
