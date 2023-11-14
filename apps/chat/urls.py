94b52c3fbb5f9f0e5fa080f2d7a4a0983c06fa48
Timo Gronau <timogronau@gmail.com>
2023-11-14 16:11:00 +0100

    chat done

--- apps/chat/urls.py
+++ apps/chat/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from . import views
-
+from apps.chat import views
+ 
+ 
 urlpatterns = [
-    ]
+    path("inbox/", views.inbox, name="inbox"),
+    path("room/<str:pk>", views.private_chat_view, name="room"),
+]
