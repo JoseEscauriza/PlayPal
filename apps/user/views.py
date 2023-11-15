5bd51af5edbeac10624ab98a8131097bf45fc68a
Jose Escauriza <escaurizajose@gmail.com>
2023-11-15 11:15:03 +0100

    progress

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,12 +1,19 @@
 from typing import Optional
 from datetime import date, datetime, timedelta
 
+from django import forms
+from django.urls import reverse_lazy
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.decorators import login_required
 
 from .models import CustomUser
-from .forms import CustomAuthenticationForm
+from .forms import CustomAuthenticationForm, CustomUserCreationForm
+
+# FOR CLASS BASED VIEWS
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView, TemplateView
 
 
 def login_view(request):
@@ -82,6 +89,13 @@ def user_profile_own(request):
         "children_data": children_data
     }
     return render(request, "user/user_page_own.html", context)
+
+
+class UserRegister(CreateView, SuccessMessageMixin):
+    template_name = 'user/registration.html'
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    success_message = "Your profile was created successfully"
 
 
 # TODO:add to user_profile_own: edit profile, change profile picture
