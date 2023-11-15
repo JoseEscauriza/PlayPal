974c227e7543570728a57d0ba10b4ec21ab48b14
sophkees1 <sophiekeesing01@gmail.com>
2023-11-15 09:18:08 +0100

    creating other user profile view, adding url

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/success/', views.logged_view, name='logged_view'),
     path('logout/', views.user_logout, name='logout'),
     path('profile/', views.user_profile_own, name='own_profile'),
+    path('user-profile/<uuid:user_uuid>/', views.other_user_profile, name='other_user_profile'),
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,12 +1,15 @@
 from typing import Optional
 from datetime import date, datetime, timedelta
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from django.http import Http404
 
 from .models import CustomUser
 from .forms import CustomAuthenticationForm
+
+import uuid
 
 
 def login_view(request):
@@ -87,3 +90,44 @@ def user_profile_own(request):
 # TODO:add to user_profile_own: edit profile, change profile picture
 # TODO: make default look for smth for pages when user must be logged-in, otherwise /profile page fails
 
+@login_required
+def other_user_profile(request, user_uuid):
+    children_data = []
+
+    try:  
+        user = CustomUser.objects.filter(uuid=user_uuid).first()
+        
+        if user is None:
+            raise Http404("User not found.")
+    
+
+        # Loop through the children
+        for child in user.child_set.all():
+            child_data = {
+                "first_name": child.first_name,
+                "gender": child.gender_id.gender_name,
+                "age": (date.today() - child.birthdate).days // 365,
+                "bio": child.bio,
+                "avatar": child.avatar.url if child.avatar else None,
+                "interests": ', '.join([interest.interest_name for interest in child.interest_id.all()]),
+                "child_images": [picture.picture.url for picture in child.pictures.all()],
+            }
+
+            children_data.append(child_data)
+
+        context = {
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "gender": user.gender,
+            "verified_status": user.verified_status,
+            "bio": user.bio,
+            "location": user.location,
+            "birthdate": user.birthdate,
+            "marital_status": user.marital_status,
+            "avatar": user.avatar.url if user.avatar else None,
+            "children_data": children_data
+        }
+        return render(request, "user/other_user_profile.html", context)
+    
+    except ValueError:
+        raise Http404("Invalid UUID format.")
