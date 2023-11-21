106ab6aab446e0aa3101343401714f250da98294
bobilan <bobilan@gmail.com>
2023-11-21 13:37:25 +0100

    remove redundant imports

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,7 +1,6 @@
 from typing import Optional
-from datetime import date, datetime, timedelta
+from datetime import date
 
-from django.db.models import OuterRef, Subquery
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
